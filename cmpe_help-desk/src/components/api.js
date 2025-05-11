ef1ad1291552f400dcbd6b24d96215659a48e0fb
Rohan Aren <rohanaren@Rohans-MacBook-Pro-2.local>
2025-05-10 19:13:09 -0700

    adding utility component for api communication

---
+++ cmpe_help-desk/src/components/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:5000'; // Flask server
+
+export const getTickets = () => axios.get(`${BASE_URL}/tickets`);
+export const getTicketById = (id) => axios.get(`${BASE_URL}/tickets/${id}`);
+export const createTicket = (data) => axios.post(`${BASE_URL}/tickets`, data);
+export const updateTicket = (id, data) => axios.put(`${BASE_URL}/tickets/${id}`, data);
+export const deleteTicket = (id) => axios.delete(`${BASE_URL}/tickets/${id}`);
