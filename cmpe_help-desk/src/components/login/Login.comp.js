9febf6667befd15830cb01221fa23f3624cda752
Taha k <tahakam2@gmail.com>
2025-05-12 11:39:19 -0700

    auth0

--- cmpe_help-desk/src/components/login/Login.comp.js
+++ cmpe_help-desk/src/components/login/Login.comp.js
@@ -1,29 +1,94 @@
+// import { Container, Row, Col, Form, FormGroup, Button } from 'react-bootstrap'
+// import PropTypes from 'prop-types'
+// import React, { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+
+// export const Login = ({handleOnchange,formSwitcher, email, pass}) => {
+//     const [username, setUsername] = useState('');
+//     const [role, setRole] = useState('');
+//     const navigate = useNavigate();
+
+//     const handleOnsubmit = (e) => {
+//         setRole('')
+//         e.preventDefault();
+        
+//         sessionStorage.setItem('username', username); // clears when tab is closed
+//         if (username === 'admin@email.com') {
+//             setRole('admin');
+//             sessionStorage.setItem('role', 'admin');
+//           } else {
+//             setRole('user');
+//             sessionStorage.setItem('role', 'user');
+//           }
+//         console.log('Username:', username);
+//         console.log('Role:', role, sessionStorage.getItem('role'))
+//         navigate('/tickets');
+//       };
+
+//   return (
+//     <Container>
+//         <Row>
+//             <Col>
+//             <h1>Client Login </h1>
+//             <hr /> 
+//             <Form onSubmit={handleOnsubmit}>
+//                 <Form.Group>
+//                     <Form.Label>Email Address</Form.Label>
+//                     <Form.Control
+//                         type="email"
+//                         name="email"
+//                         value = {email}
+//                         onChange={(e) => setUsername(e.target.value)}
+//                         placeholder= "Enter your Email"
+//                         required/>
+//                 </Form.Group>
+//                 <Form.Group>
+//                     <Form.Label className='mt-1'>Password</Form.Label>
+//                     <Form.Control
+//                         type="password"
+//                         name="password"
+//                         value={pass}
+//                         placeholder= "Enter your Password"
+//                         required
+//                     />
+//                 </Form.Group>
+//                 <Button className = "mt-3" type="submit"> Login </Button>
+//             </Form>
+//             <hr />
+//             </Col>
+//         </Row>
+//         <Row>
+//             <Col>
+//             <a href ="#!" onClick={() => formSwitcher('reset')}>Forgot Password?</a>
+//             </Col>
+//         </Row>
+//     </Container>
+//   )
+// }
+
+// Login.propTypes ={
+//     handleOnchange: PropTypes.func.isRequired,
+//     handleOnsubmit: PropTypes.func.isRequired,
+//     formSwitcher: PropTypes.func.isRequired,
+//     email: PropTypes.string.isRequired,
+//     pass: PropTypes.string.isRequired
+// };
+
+import React from 'react'
 import { Container, Row, Col, Form, FormGroup, Button } from 'react-bootstrap'
 import PropTypes from 'prop-types'
-import React, { useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
 
-export const Login = ({handleOnchange,formSwitcher, email, pass}) => {
-    const [username, setUsername] = useState('');
-    const [role, setRole] = useState('');
-    const navigate = useNavigate();
+export const Login = ({handleOnchange,handleOnsubmit,formSwitcher, email, pass}) => {
+    const navigate = useNavigate()
 
-    const handleOnsubmit = (e) => {
-        setRole('')
-        e.preventDefault();
-        
-        sessionStorage.setItem('username', username); // clears when tab is closed
-        if (username === 'admin@email.com') {
-            setRole('admin');
-            sessionStorage.setItem('role', 'admin');
-          } else {
-            setRole('user');
-            sessionStorage.setItem('role', 'user');
-          }
-        console.log('Username:', username);
-        console.log('Role:', role, sessionStorage.getItem('role'))
-        navigate('/tickets');
-      };
+    const { loginWithRedirect, isAuthenticated, user, logout, getAccessTokenSilently } = useAuth0();
+
+    const handleClick =() => {
+        navigate('/dashboard')
+    }
+
 
   return (
     <Container>
@@ -31,29 +96,21 @@ export const Login = ({handleOnchange,formSwitcher, email, pass}) => {
             <Col>
             <h1>Client Login </h1>
             <hr /> 
-            <Form onSubmit={handleOnsubmit}>
-                <Form.Group>
-                    <Form.Label>Email Address</Form.Label>
-                    <Form.Control
-                        type="email"
-                        name="email"
-                        value = {email}
-                        onChange={(e) => setUsername(e.target.value)}
-                        placeholder= "Enter your Email"
-                        required/>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label className='mt-1'>Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        name="password"
-                        value={pass}
-                        placeholder= "Enter your Password"
-                        required
-                    />
-                </Form.Group>
-                <Button className = "mt-3" type="submit"> Login </Button>
-            </Form>
+
+            {!isAuthenticated ? (
+            <Button onClick={() => loginWithRedirect()}>
+              Login
+            </Button>
+          ) : (
+            <>
+              <p>Welcome, {user.name}!</p>
+              <div><Button  onClick={handleClick}>To the DashBoard</Button></div>
+
+              <Button className="mt-2" onClick={() => logout({ returnTo: window.location.origin })}>
+                Logout
+              </Button>
+            </>
+          )}
             <hr />
             </Col>
         </Row>
